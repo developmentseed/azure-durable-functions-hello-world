c748efbd6336e2cf1056b995bb31d911f7e67404
Aimee Barciauskas <aimee@developmentseed.org>
2019-06-20 00:26:25 -0400

    Minor changes for ingest

--- app/ingest/index.js
+++ app/ingest/index.js
@@ -1,7 +1,25 @@
+const request = require('request');
+
 module.exports = function(context, message) {
-  context.log('Node.js queue trigger function processed work item', message);
+  context.log('Node.js queue trigger function processed work item: ', message);
   // OR access using context.bindings.<name>
-  // context.log('Node.js queue trigger function processed work item', context.bindings.myQueueItem);  
-  context.bindings.myOutputBlob = message;
-  context.done();
+  // context.log('Node.js queue trigger function processed work item', context.bindings.myQueueItem);
+  const options = {
+    url: message,
+    auth: {
+      'user': 'aimeeb',
+      'pass': process.env.WEBDAV_PASSWORD
+    }
+  };
+  
+  context.log('Starting request');
+  return request.get(options, function (error, response, body) {
+    context.log('statusCode:', response && response.statusCode);
+    if (err) {
+      context.error('error:', error); // Print the error if one occurred
+      return err;
+    };
+    context.bindings.myOutputBlob = body;
+    context.done();
+  });
 };
